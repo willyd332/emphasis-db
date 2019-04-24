5611cedf5ade86e8e670b3ec81e41f291825cd3e
WillyD <sopwill3@gmail.com>
2019-04-24 12:38:29 -0600

    started making stringParser, using recursion

---
+++ analData.js
@@ -0,0 +1,5 @@
+/* PSUESDO CODE
+
+
+
+*/
---
+++ stringParser.js
@@ -0,0 +1,73 @@
+/* PSUESDO CODE
+
+// Create Class Section {
+                  constructor(string){
+                  this.textSection: string
+                  }
+                  Analysis:{}
+                  }
+
+// Create Empty Array []
+
+// Take in string
+
+// Replace all "" with ''
+
+// Take the first 2000 characters of the string
+// (or until the end of the string)
+      // If the last character is a period:
+                // Record index number
+                // create new Section with string
+                // push Section into array
+      // If the last character is not a period:
+                // Move backwards until the closest period
+                // Record index number
+                // create new Section with string
+                // push Section into array
+
+// Repeat the process with the 2000 characters following
+// the recorded index of the period (or until the end of the string)
+
+// Return array of objects
+
+*/
+
+"use strict";
+
+class Section {
+  constructor(str) {
+    this.text     = str;
+    this.analysis = {};
+  }
+}
+
+const stringParser = (str) => {
+
+const sectionArr = [];
+const string = str.replace(/\"/g,"'");
+
+    const rStringParser = (string, index = 2000)=>{
+
+      let sectionString    = "";
+
+      if ((string.length - index) < 2000){
+        for (let i = index + 1; i < string.length; i++){
+          sectionString += string[i];
+        }
+        sectionArr.push(new Section(sectionString));
+        return sectionArr
+      }
+
+
+      return rStringParser(string,index)
+    }
+
+if (string.length >= 2000){
+  return rStringParser();
+} else {
+  sectionArr.push(new Section(string));
+  return sectionArr
+}
+
+
+}
