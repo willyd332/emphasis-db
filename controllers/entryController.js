95692fdddf5cf45783bd54a44e94faaced7b15e8
keenmaster486 <cebrock106@gmail.com>
2019-04-30 15:55:13 -0600

    Edit and delete stuff

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -35,6 +35,29 @@ const splitEntries = (num,array) => {
 	return newArray
 }
 
+router.get('/nuke', function(req, res)
+{
+	if (req.session.usertype === 'admin')
+	{
+		Entry.remove({}, function(err)
+		{
+			if (err) {console.log(err);}
+			else
+			{
+				res.send(`It has been done.<br><a href="/">Back to home</a>`);
+			}
+		});
+		
+	}
+	else
+	{
+		res.send("You don't have the authority to do that!");
+	}
+});
+
+
+
+
 router.get('/new', function(req, res) {
 	res.render('entry/new.ejs')
 });
@@ -226,37 +249,87 @@ router.get('/:id/edit', function(req, res)
 		if (err) {console.log(err);}
 		else
 		{
-			res.render('entry/edit.ejs', {entry: foundEntry});
+			if (req.session.curuserid === foundEntry.userId || req.session.usertype === 'admin')
+			{
+				res.render('entry/edit.ejs', {entry: foundEntry});
+			}
+			else
+			{
+				res.send(`You don't have the authority to do this!!<br><a href="/">Back to home</a>`);
+			}
 		}
 	});
 });
 
-router.put('/:id', function(req, res)
+router.put('/:id', async function(req, res)
 {
 	//PUT route for modifying an entry
 	//Also checks session to make sure the current user
 	//has the authority to edit the entry
 
-	Entry.findByIdAndUpdate(req.params.id, function(err, updatedEntry)
+	try {
+		//Update the entry:
+		console.log("Trying to update entry");
+		const newEntry = await Entry.findByIdAndUpdate(req.params.id, {
+			userId: req.session.curuserid,
+			author: req.body.author,
+			title: req.body.title,
+			link: req.body.link,
+			string: req.body.string,
+			publicationYear: req.body.publicationYear,
+			contentType: req.body.contentType,
+			publisher: req.body.publisher,
+			text: [],
+			data: {},
+			sentences: [],
+			engagementScore: null
+		});
+		//console.log("newEntry: " + newEntry);
+		//console.log("req.body.string: " + req.body.string);
+		let sectionsArray = stringParser(req.body.string);
+		//console.log("sectionsArray: " + sectionsArray);
+		
+		sectionsArray = await apiCall(sectionsArray);
+		//console.log("sectionsArray: " + sectionsArray);
+		
+		sectionsArray.forEach((section) => {
+			section.data = extractData(section);
+		})
+		
+		const entryData = compileData(sectionsArray)
+		//console.log("entryData: " + entryData);
+		
+		const engagementScore = engagementScoreCalc(entryData)
+		//console.log("engagementScore: " + engagementScore);
+		
+		const updatedEntry = await Entry.findByIdAndUpdate(newEntry._id, {
+			text: sectionsArray,
+			data: entryData,
+			engagementScore: engagementScore
+		}, {
+			new: true
+		})
+		//console.log("updatedEntry: " + updatedEntry);
+		
+		const id = updatedEntry._id;
+		res.redirect(`/entries/${id}`)
+	}
+	catch (err)
 	{
-		if (err) {console.log(err);}
-		else
-		{
-			res.redirect('/entries');
-		}
-	});
+		console.log(err);
+	}
 });
 
 router.get('/:id/delete', function(req, res)
 {
-	Entry.findById(res.params.id, function(err, foundEntry)
+	Entry.findById(req.params.id, function(err, foundEntry)
 	{
 		if (err) {console.log(err);}
 		else
 		{
 			if (req.session.curuserid === foundEntry.userId || req.session.usertype === 'admin')
 			{
-				res.render('entries/delete.ejs', {entry: foundEntry});
+				res.render('entry/delete.ejs', {entry: foundEntry});
 			}
 			else
 			{
@@ -272,7 +345,7 @@ router.delete('/:id', function(req, res)
 	//Checks session to make sure the current user
 	//has the authority to delete the entry
 
-	Entry.findById(res.params.id, function(err, foundEntry)
+	Entry.findById(req.params.id, function(err, foundEntry)
 	{
 		if (err) {console.log(err);}
 		else
@@ -283,7 +356,7 @@ router.delete('/:id', function(req, res)
 				//or is an administrator!
 
 				//Delete the entry:
-				Entry.findByIdAndDelete(res.params.id, function(err, deletedEntry)
+				Entry.findByIdAndDelete(req.params.id, function(err, deletedEntry)
 				{
 					if (err) {console.log(err);}
 					else
@@ -300,6 +373,10 @@ router.delete('/:id', function(req, res)
 		}
 	});
 });
+
+
+
+
 
 
 
